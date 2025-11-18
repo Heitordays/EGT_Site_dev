92bd8cd0faee52fae07a1aed3c81f46731c10472
Heitordays <hei.de.todos@gmail.com>
2025-11-18 16:36:40 -0300

    Adiciona

--- src/assets/Components/Header/Header.jsx
+++ src/assets/Components/Header/Header.jsx
@@ -1,8 +1,9 @@
-// src/components/Header/Header.jsx
+// src/assets/Components/Header/Header.jsx
 
 import React from 'react';
 import './Header.css';
-// import './Header.css'; // Não se esqueça de descomentar a importação dos estilos
+
+import LanguageSwitchButton from '../LanguageSwitchButton/LanguageSwitchButton.jsx';
 
 const Header = ({ headerClasses }) => {
     // Nota: A lógica de scroll (scrolled/hide-on-scroll) fica no componente que chama o Header.
@@ -13,23 +14,21 @@ const Header = ({ headerClasses }) => {
                 <a href="./">
                     <img src="logo.png" className="logo" />
                 </a>
-                {/* O seu menu principal agora dividido em duas ULs */}
                 <div className="navbar-links">
                     <ul className="menu">
-                        <li><a href="industria">Industria</a></li>
+                        <li><a href="industria">Indústria</a></li>
                         <li><a href="projetos">Projetos</a></li>
-                        <li><a href="sobre-nos">Sobre nos</a></li>
+                        <li><a href="sobre-nos">Sobre nós</a></li>
                         <li><a href="carreira">Carreira</a></li>
                         <li><a href="servicos">Serviços</a></li>
                         <li><a href="publicacoes">Publicações</a></li>
-                        <li><a href="contate-nos">Contate-nos!</a></li>
+                        <li><a href="contate-nos" className="cta-button">Contate-nos!</a></li>
                     </ul>
                 </div>
             </div>
 
             <div className="navbar-right">
-                <span className="lang">pt-br ▼</span>
-                <a href="mailto:contato@egt.com"><div className="circle-icon">📧</div></a>
+                <LanguageSwitchButton />
                 <a href="#"><div className="circle-icon">🔍</div></a>
             </div>
         </header>
