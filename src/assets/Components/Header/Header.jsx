529f91154c35032ffab98c0f51952ba1a687a814
Heitordays <hei.de.todos@gmail.com>
2025-11-19 17:07:53 -0300

    Organizção do grid e do css do header

--- src/assets/Components/Header/Header.jsx
+++ src/assets/Components/Header/Header.jsx
@@ -6,28 +6,28 @@ import './Header.css';
 import LanguageSwitchButton from '../LanguageSwitchButton/LanguageSwitchButton.jsx';
 
 const Header = ({ headerClasses }) => {
-    // Nota: A lógica de scroll (scrolled/hide-on-scroll) fica no componente que chama o Header.
-
     return (
         <header className={headerClasses || 'navbar'}>
             <div className="navbar-left">
                 <a href="./">
-                    <img src="logo.png" className="logo" />
+                    <img src="logo.png" className="logo" alt="Logo" />
                 </a>
                 <div className="navbar-links">
                     <ul className="menu">
+                        {/* 6 Links que se encaixam no layout de 4 colunas (4 na 1ª linha, 2 na 2ª) */}
                         <li><a href="industria">Indústria</a></li>
                         <li><a href="projetos">Projetos</a></li>
                         <li><a href="sobre-nos">Sobre nós</a></li>
                         <li><a href="carreira">Carreira</a></li>
                         <li><a href="servicos">Serviços</a></li>
                         <li><a href="publicacoes">Publicações</a></li>
-                        <li><a href="contate-nos" className="ctt-button">Contate-nos!</a></li>
+                        {/* ⚠️ REMOVIDO: O botão Contate-nos! não deve estar aqui. */}
                     </ul>
                 </div>
             </div>
 
             <div className="navbar-right">
+                <a href="contate-nos" className="ctt-button">Contate-nos!</a> 
                 <LanguageSwitchButton />
                 <a href="#"><div className="circle-icon">🔍</div></a>
             </div>
